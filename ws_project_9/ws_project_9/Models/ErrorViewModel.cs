61cff1b44ccfab6c174ae78df396e257808a5685
RALSURDEH <RALSURDEH@EB146-01>
2018-09-18 09:41:54 +1000

    add project

---
+++ ws_project_9/ws_project_9/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ws_project_9.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
